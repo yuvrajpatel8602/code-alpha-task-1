a8f7a6d28e8babc910d5cb43aa0638099aa22648
Yuvraj Patel <yuvrajsinghpatel8602@gmail.com>
2025-07-21 01:17:05 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,15 @@
+def chatbot():
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Bot: Hi!")
+        elif user_input == "how are you":
+            print("Bot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: Sorry, I don't understand.")
+
+chatbot()
